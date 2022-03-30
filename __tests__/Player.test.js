7713477448aaee55dea2b8d3abbfd1f3e7118826
Kelsey Alderman <kelseyalderman@gmail.com>
2022-03-29 17:59:04 -0700

    getStats and getInventory methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,7 @@
-const Potion = require("../lib/Potion");
+const Player = require("../lib/Player.js");
+const Potion = require("../lib/Potion.js");
 
-jest.mock("../lib/Potion");
-
-console.log(new Potion());
-
-const Player = require("../lib/Player");
+jest.mock("../lib/Potion.js");
 
 test("creates a player object", () => {
   const player = new Player("Dave");
@@ -16,4 +13,23 @@ test("creates a player object", () => {
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player("Dave");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
